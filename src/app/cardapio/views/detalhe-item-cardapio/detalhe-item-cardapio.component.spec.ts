3a3f38eddf8fc55ca7ebe7bb9c993d299888ddf5
Allan Oliveira <allan_rj5@hotmail.com>
2024-01-25 13:01:32 -0300

    feat: adicionando as opções de colocar mais de um item e adicionar ao pedido

--- src/app/cardapio/views/detalhe-item-cardapio/detalhe-item-cardapio.component.spec.ts
+++ src/app/cardapio/views/detalhe-item-cardapio/detalhe-item-cardapio.component.spec.ts
@@ -5,7 +5,10 @@ import {
   MAT_BOTTOM_SHEET_DATA,
   MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CardapioModule } from '../../cardapio.module';
+import { Util } from '../../../utils/util';
+import { NgxsModule } from '@ngxs/store';
 
 describe('DetalheItemCardapioComponent', () => {
   let component: DetalheItemCardapioComponent;
@@ -18,14 +21,19 @@ describe('DetalheItemCardapioComponent', () => {
     } as unknown as jest.Mocked<MatBottomSheetRef>;
     TestBed.configureTestingModule({
       declarations: [DetalheItemCardapioComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [ReactiveFormsModule, CardapioModule, NgxsModule.forRoot()],
       providers: [
+        FormBuilder,
         {
           provide: MatBottomSheetRef,
           useValue: bottomSheetRefMock,
         },
         {
           provide: MAT_BOTTOM_SHEET_DATA,
+          useValue: {},
+        },
+        {
+          provide: Util,
           useValue: {},
         },
       ],
@@ -41,11 +49,26 @@ describe('DetalheItemCardapioComponent', () => {
   });
 
   it('deve fechar o componente MatBottomSheetRef corretamente', () => {
-    const eventMock: Partial<MouseEvent> = { preventDefault: jest.fn() };
-
-    component.fechar(eventMock as MouseEvent);
+    component.fechar();
 
     expect(bottomSheetRefMock.dismiss).toHaveBeenCalled();
-    expect(eventMock.preventDefault).toHaveBeenCalled();
+  });
+
+  it('deve ter 3 item após executar o método "adicionar" duas vezes', () => {
+    for (let index = 0; index < 2; index++) {
+      component.adicionar();
+    }
+
+    expect(component.quantidadeAtual).toEqual(3);
+  });
+
+  it('deve ter 1 item após executar o método "remover" duas vezes', () => {
+    component.quantidadeAtual = 3;
+
+    for (let index = 0; index < 2; index++) {
+      component.remover();
+    }
+
+    expect(component.quantidadeAtual).toEqual(1);
   });
 });
